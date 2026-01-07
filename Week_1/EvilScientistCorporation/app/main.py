0cea253823c454853db78bbe4240dd187f98faa2
James Nguyen <jamesnguyen@Jamess-MacBook-Air.local>
2026-01-07 10:34:34 -0600

    new updates

--- Week_1/EvilScientistCorporation/app/main.py
+++ Week_1/EvilScientistCorporation/app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from app.routers import users
 
@@ -6,6 +8,13 @@ from app.routers import users
 
 app = FastAPI()
 
+# Global custom Exception Handler 
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request, exception: HTTPException):
+    return JSONResponse(
+        status_code=exception.status_code,
+        content={"message": exception.detail},
+    )
 # Import routers here
 app.include_router(users.router)
 # Generic sample endpoint (GET request that just returns a message)
